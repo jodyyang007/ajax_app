5ad49cb507b8929f307464d1d2ca21ae086d548a
jodyyang007 <yangjuanjuanjody89@gmail.com>
2023-07-09 12:50:24 +0900

    コントローラーにリクエストを送信する

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #get 'posts', to: 'posts#index'  # posts#indexへのパスを、ルートパスに変更し編集する
+  #get 'posts/new', to: 'posts#new'  # 新規投稿ページへの遷移は行わないため削除する 
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
 end
